0fabfa604922bc953b3a0db9d13b9631d77c9643
yunesmchaar <yunes.mchaar@gmail.com>
2024-09-17 19:06:16 +0100

    My first code python

--- cours/test.py
+++ cours/test.py
@@ -1,1 +1,42 @@
-print("hello word")
+
+#This is my first Python program
+
+print("I like pizza!")
+print("It's really good!")
+
+# variable = A container for a value (string,integer, float,boolean)
+#A variable behaves as if it was the value it contains
+#String
+first_name = "YUNESS"
+food="pizza"
+email= "yunes.mchaar@gmail.com"
+#Integers
+age = 25
+quantity = 3
+num_of_students = 30
+
+print(f"Hello {first_name}")
+print(f"You like {food}")
+print(f"Your email is:{email}")
+print(f"You are :{age}")
+print(f"You are:{quantity} items")
+print(f"Your class has {num_of_students} students")
+
+# float
+
+price = 10.99
+gpa = 3.2
+distance = 5.5
+
+print(f"The price is {price}$")
+print(f"Your gpa is: {gpa}")
+print(f"Your distance is:{distance}km")
+
+
+
+
+
+
+
+
+
