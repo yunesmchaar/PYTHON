e171c62cfe74bd388220e39afc15323eeee31619
yunesmchaar <yunes.mchaar@gmail.com>
2024-09-19 11:15:32 +0100

    My first app

--- cours/test.py
+++ cours/test.py
@@ -91,6 +91,18 @@ area=length*width
 
 print(f"The area is: {area}cm²")
 
+#Exercise 2 Shopping Cart Program
+
+item = input("What item would you like to buy?: ")
+price = float(input ("What is the price?:"))
+quantity = int(input("How many would you like?:"))
+total = price * quantity
+print(f"You have bought {quantity} x {item}/s")
+print(f"Your total is: {total}$")
+
+
+
+
 
 
 
