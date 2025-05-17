3bf1ec3d25b12eed9020e1278e3b93c82f140717
yunesmchaar <yunes.mchaar@gmail.com>
2025-05-17 16:57:24 +0100

    HELLO

--- Le170525/Newrandom.py
+++ Le170525/Newrandom.py
@@ -2,7 +2,7 @@ import random
 
 valeur=random.random()
 
-print(f"Votre valeur est {valeur:.2f}")
+print(f"Votre valeur est {valeur:.3f}")
 if valeur <0.2 :
       print("Bravo!")
 else:
---
+++ Le170525/newUniform.py
@@ -0,0 +1,4 @@
+import random
+
+valeur=random.uniform(-5,35)
+print(f"la temperature d'aujourd hui est de {valeur :.2f} C")
