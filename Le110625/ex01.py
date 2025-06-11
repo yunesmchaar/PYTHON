709103703c335a585c340cb07754c97a5cdc7ba9
yunesmchaar <yunes.mchaar@gmail.com>
2025-06-11 14:27:44 +0100

    HELLO

---
+++ Le110625/ex01.py
@@ -0,0 +1,14 @@
+print("HELLO ,I am Python")
+
+import random
+import numpy as np
+import matplotlib.pyplot as plt
+
+noms=['A','B','C','D','E']
+notes=[10,18,13,16,11]
+
+plt.plot(noms,notes)
+plt.title("Rappele")
+plt.xlabel('Axe x')
+plt.ylabel('Axe y')
+plt.show()
